deedb4f88f189fbbe1d75c31855ec21839f9fad9
piotraron <piotraron@gmail.com>
2021-09-25 03:18:03 +0100

    commands.js updated

--- commands/commands.js
+++ commands/commands.js
@@ -18,7 +18,8 @@ module.exports = {
                 { name: '!collectors', value: `Number of unique owners`},
                 { name: `!${slug} NUMBER`, value: `Detailed information about Your ${slug}`},
                 { name: '!auction', value: `Auctions explained`},
-                { name: "!trait VALUE", value: `Filter for trait VALUE. Shows some random selection.`}
+                { name: '!showme ADDRESS', value: `Displays all ${slug}s of the address owner`},
+                { name: "!trait VALUE", value: `Filter for trait VALUE. Shows a random selection.`}
 
 
             )
