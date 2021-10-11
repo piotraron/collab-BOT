6c4a78cf26bfb0088091e645ff3efef3e4665c9b
piotraron <piotraron@gmail.com>
2021-10-11 21:36:10 +0100

    updated jons and !GM added

---
+++ commands/gm.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	name: "gm",
+	execute(message) {
+
+            const embedMsg = new Discord.MessageEmbed()
+            .setImage("https://lh3.googleusercontent.com/OoWeebTJCmCfYySgX6Pwiw1OH0HwOyyPiXriZsr6MHlgjFP9Su4g2oMCDM6MkozcQ4XbNte6rD0MtKOz6yG6vMxtu_4EP1nnEbH7bw")
+          
+            message.channel.send(embedMsg)
+
+        .catch(error => message.channel.send(error.message));
+    }
+}
